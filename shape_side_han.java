f2e6d32526a86d13a1d6660533689c65d3fb8615
brent-sheltz <35032261+brent-sheltz@users.noreply.github.com>
2018-01-12 10:54:52 -0500

    Add files via upload

---
+++ shape_side_han.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class shape_side_han here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class shape_side_han
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class shape_side_han
+     */
+    public shape_side_han()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
